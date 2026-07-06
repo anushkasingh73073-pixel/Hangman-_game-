1a5c16cd66b58a1ebdb52b673fbff23f961c6248
Annu <anushkasingh73073@gmail.com>
2026-07-06 10:48:51 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,65 @@
+import random
+
+# Word list
+words = ["earth", "mars", "jupiter", "saturn",
+         "uranus", "neptune", "mercury", "venus"]
+
+# Random word selection
+word = random.choice(words)
+
+# Blank spaces
+display = ["_"] * len(word)
+
+# Store guessed letters
+letters = []
+
+# Total chances
+chance = 6
+
+print("=== HANGMAN GAME ===")
+print("Guess letters from the SOLAR SYSTEM")
+
+# Game loop
+while chance > 0 and "_" in display:
+
+    print("\nWord:", " ".join(display))
+    print("Chances left:", chance)
+    print("Guessed letters:", " ".join(letters))
+
+    # User input
+    guess = input("Enter a letter: ").lower()
+
+    # Check valid input
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter only one alphabet.")
+        continue
+
+    # Already guessed
+    if guess in letters:
+        print("You already guessed this letter.")
+        continue
+
+    letters.append(guess)
+
+    # Correct guess
+    if guess in word:
+        print("Correct Guess!")
+
+        for i in range(len(word)):
+            if word[i] == guess:
+                display[i] = guess
+
+    # Wrong guess
+    else:
+        print("Wrong Guess!")
+        chance -= 1
+
+# Win condition
+if "_" not in display:
+    print("\n🎉 Congratulations! You Won!")
+    print("The word was:", word)
+
+# Lose condition
+else:
+    print("\n💀 Game Over!")
+    print("The word was:", word)
